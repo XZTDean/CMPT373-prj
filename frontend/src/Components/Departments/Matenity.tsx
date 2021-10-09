2a6276bbc5e2f575d66d306c8c0a74da04c4e16a
jza229 <jza229@sfu.ca>
2021-10-08 20:27:36 -0700

    Maternity page init

--- frontend/src/Components/Departments/Matenity.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function Matenity() {
-    return (
-        <div>Jump to Matenity Home Page</div>
-    )
-}
-
-export default Matenity;
---
+++ frontend/src/Components/Departments/Maternity.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import '../../Departments.css'
+
+function Maternity() {
+    return (
+        <div id="wrapper">
+            <h1 text-align="center">Maternity Home Page</h1>
+            <div id="Vcontainer">
+                <div id="caseStudy" className="with_border">Case Study</div>
+                <div id="Data" className="with_border">Data</div>
+                <div id="empofMth" className="with_border">Employee Of Month</div>
+            </div>
+        </div>
+    );
+}
+
+export default Maternity;
