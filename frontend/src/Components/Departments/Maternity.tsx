62fa58a725a7a0f1bc14424ca6b1f8339c6d691f
jza229 <jza229@sfu.ca>
2021-10-09 14:51:11 -0700

    Before fectch frontend update

--- frontend/src/Components/Departments/Maternity.tsx
+++ frontend/src/Components/Departments/Maternity.tsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import '../../Departments.css'
 import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
+// import '../../bootstrap5/bootstrap-5.0.0-beta1-dist/bootstrap.min.css';
+// import '../../bootstrap5/bootstrap-5.0.0-beta1-dist/bootstrap.bundle.min.js';
+//             <div className="text-center text-success fs-1">看到我居中，颜色绿色表示起作用了！</div>
 
 function Maternity() {
     return (
         <div id="wrapper">
+        <div className="text-center text-success fs-1">看到我居中，颜色绿色表示起作用了！</div>
             <h1 id="title">Maternity Home Page</h1>
             <div id="Hcontainer">
                 <div id="caseStudy" className="with_border">
@@ -29,7 +33,7 @@ function Maternity() {
                             <Link to="/maternity/empofmthpg">Employee Of Month</Link>
                         </button>
                     </Router>
-                    <div className="inner_mssg">Next Case Study will Due on:</div>
+                    <div className="inner_mssg">Some employees</div>
                 </div>
                 <div id="bioMech" className="with_border">
                     <Router>
@@ -40,7 +44,7 @@ function Maternity() {
                     <div className="inner_mssg">No message</div>
                 </div>
                 <div id="pointsTally" className="with_border">
-                    <div>Your current points tally is:</div>
+                    <div id="pointsTally_header">Your current points tally is:</div>
                     <div className="inner_mssg">employee_score</div>
                 </div>
             </div>
