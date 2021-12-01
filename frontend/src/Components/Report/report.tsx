bd0a9a23dd982a6ee9322655415aced7aa2b088c
Zeti Xiong <zetix@sfu.ca>
2021-11-30 20:55:14 -0800

    Add function to parse response body

--- frontend/src/Components/Report/report.tsx
+++ frontend/src/Components/Report/report.tsx
@@ -1,25 +1,30 @@
 export interface ReportMetadata {
-    id: number,
-    name: string,
-    departmentId: number,
-    departmentName: string,
-    month: string,
-    createdAt: number,
-    editedAt: number,
-    createdBy: string,
-    editedBy: string,
+    id?: number,
+    name?: string,
+    departmentId?: number,
+    departmentName?: string,
+    month?: string,
+    createdAt?: number,
+    editedAt?: number,
+    createdBy?: string,
+    editedBy?: string,
 }
 
 export interface ReportData {
     id: number,
-    questions: Question,
+    questions: Question[],
     groupings: string
+}
+
+export interface Report {
+    data: ReportData,
+    metadata: ReportMetadata,
 }
 
 export interface Question {
     id: number,
-    createdAt: number,
-    editedAt: number,
+    createdAt?: number,
+    editedAt?: number,
     departmentId: number,
     group: string,
     order: number,
@@ -27,4 +32,39 @@ export interface Question {
     answer: string,
     choices: string,
     type: string
+}
+
+export function responseToReport(r: any): Report {
+    let metadata: ReportMetadata = {
+        id: r.id,
+        name: r.name,
+        month: r.month,
+        departmentId: r.departmentId,
+        departmentName: r.departmentName,
+        createdAt: r.createdAt,
+        createdBy: r.createdBy,
+        editedAt: r.editedAt,
+        editedBy: r.editedBy
+    }
+    let data: ReportData = {
+        id: r.id,
+        questions: [],
+        groupings: r.groupings
+    }
+    r.questions.forEach((q: any) => {
+        let question: Question = {
+            answer: q.answer,
+            choices: q.choices,
+            createdAt: q.createdAt,
+            departmentId: q.departmentId,
+            editedAt: q.editedAt,
+            group: q.group,
+            order: q.order,
+            question: q.question,
+            type: q.type,
+            id: q.id
+        }
+        data.questions.push(question);
+    })
+    return {metadata: metadata, data: data}
 }
